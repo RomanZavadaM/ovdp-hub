fd79fa1796325e3572a63dd689da0995af7ae3e4
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-24 17:33:37 +0300

    Vault: isolate Windows record store from Win32 binding

--- apps/native/lib/security/vault_device_key_store_windows.dart
+++ apps/native/lib/security/vault_device_key_store_windows.dart
@@ -1,89 +1,15 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:ffi/ffi.dart';
 import 'package:path/path.dart' as p;
-import 'package:win32/win32.dart';
 
 import 'vault_crypto.dart';
 import 'vault_device_key_store.dart';
 
-// Win32 DPAPI flag documented by CryptProtectData/CryptUnprotectData.
-const int _cryptProtectUiForbidden = 0x1;
-
 abstract interface class DpapiProtector {
   Uint8List protect(Uint8List plainText);
   Uint8List unprotect(Uint8List cipherText);
-}
-
-class Win32DpapiProtector implements DpapiProtector {
-  void _ensureWindows() {
-    if (!Platform.isWindows) {
-      throw UnsupportedError('vault.dpapi_windows_only');
-    }
-  }
-
-  @override
-  Uint8List protect(Uint8List plainText) {
-    _ensureWindows();
-    return _transform(plainText, protect: true);
-  }
-
-  @override
-  Uint8List unprotect(Uint8List cipherText) {
-    _ensureWindows();
-    return _transform(cipherText, protect: false);
-  }
-
-  Uint8List _transform(Uint8List input, {required bool protect}) =>
-      using((arena) {
-        if (input.isEmpty) {
-          throw const FormatException('vault.dpapi_empty_input');
-        }
-        final inputBytes = arena<Uint8>(input.length);
-        inputBytes.asTypedList(input.length).setAll(0, input);
-        final inputBlob = arena<CRYPT_INTEGER_BLOB>();
-        inputBlob.ref.cbData = input.length;
-        inputBlob.ref.pbData = inputBytes;
-
-        final outputBlob = arena<CRYPT_INTEGER_BLOB>();
-        final result = protect
-            ? CryptProtectData(
-                inputBlob,
-                null,
-                null,
-                null,
-                _cryptProtectUiForbidden,
-                outputBlob,
-              )
-            : CryptUnprotectData(
-                inputBlob,
-                null,
-                null,
-                null,
-                _cryptProtectUiForbidden,
-                outputBlob,
-              );
-        if (!result.value) {
-          throw StateError(
-            protect ? 'vault.dpapi_protect_failed' : 'vault.dpapi_unprotect_failed',
-          );
-        }
-
-        final output = outputBlob.ref.pbData;
-        if (output.address == 0 || outputBlob.ref.cbData <= 0) {
-          throw StateError('vault.dpapi_empty_result');
-        }
-        try {
-          return Uint8List.fromList(
-            output.asTypedList(outputBlob.ref.cbData),
-          );
-        } finally {
-          LocalFree(HLOCAL(output.cast()));
-        }
-      });
 }
 
 class WindowsVaultDeviceKeyStore implements VaultDeviceKeyStore {
@@ -94,8 +20,8 @@ class WindowsVaultDeviceKeyStore implements VaultDeviceKeyStore {
 
   WindowsVaultDeviceKeyStore({
     required this.directory,
-    DpapiProtector? protector,
-  }) : protector = protector ?? Win32DpapiProtector();
+    required this.protector,
+  });
 
   File _target(String vaultId) {
     validateVaultId(vaultId);
