7a06bb0d6b31971c86eb120df1dc050c20e34190
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-24 18:32:14 +0300

    Vault: expose content-store interface for session layer

--- apps/native/lib/security/vault_store.dart
+++ apps/native/lib/security/vault_store.dart
@@ -106,9 +106,18 @@ class VaultOpenResult {
   }) : plainText = Uint8List.fromList(plainText);
 }
 
+abstract interface class VaultContentStore {
+  Future<VaultOpenResult> open({required String vaultId});
+
+  Future<VaultOpenResult> save({
+    required String vaultId,
+    required Uint8List plainText,
+  });
+}
+
 typedef VaultCommitProbe = FutureOr<void> Function(File committedFile);
 
-class LocalVaultStore {
+class LocalVaultStore implements VaultContentStore {
   final Directory directory;
   final VaultCrypto crypto;
   final VaultDeviceKeyStore deviceKeyStore;
