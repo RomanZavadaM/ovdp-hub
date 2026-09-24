5678d14d3b069fe30a2f1ec231c8bc1f88d43e0e
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-24 17:32:24 +0300

    Tests: clean device-store imports

--- apps/native/test/vault_device_key_store_test.dart
+++ apps/native/test/vault_device_key_store_test.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
